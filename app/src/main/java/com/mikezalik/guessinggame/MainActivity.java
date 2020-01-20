1618dff47ad85f66130e7193f71c7ee8d1af1a14
mikezalik <mike.zalik@gmail.com>
2020-01-20 13:38:49 -0800

    Initial commit

---
+++ app/src/main/java/com/mikezalik/guessinggame/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mikezalik.guessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    int randomNumber;
+
+    public void guess(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Random rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+    }
+}
