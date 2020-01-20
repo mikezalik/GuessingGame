9724b13ef13c2f5a25b15b8192c1f07c6ec5c76f
mikezalik <mike.zalik@gmail.com>
2020-01-20 13:44:55 -0800

    Adds import statement for guess view.

--- app/src/main/java/com/mikezalik/guessinggame/MainActivity.java
+++ app/src/main/java/com/mikezalik/guessinggame/MainActivity.java
@@ -3,6 +3,7 @@ package com.mikezalik.guessinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.*;
